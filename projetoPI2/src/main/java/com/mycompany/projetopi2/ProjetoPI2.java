6d3e77c64482956be6161306652105bd5eaa7ca7
Tiago Rabello <117934113+tirabello@users.noreply.github.com>
2023-04-04 22:01:39 -0300

    Add files via upload
    cliente

---
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/ProjetoPI2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projetopi2;
+
+/**
+ *
+ * @author tiago.rbraz
+ */
+public class ProjetoPI2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
