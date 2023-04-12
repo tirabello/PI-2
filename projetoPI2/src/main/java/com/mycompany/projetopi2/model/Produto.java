16f82bb4f1b4c8603f340ed8c76f0e760f49a723
Leonardo Silva <ldss1@outlook.com>
2023-04-11 23:50:25 -0300

    Create files MODEL

---
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/model/Produto.java
@@ -0,0 +1,15 @@
+package com.mycompany.projetopi2.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ldss1
+ */
+public class Produto {
+    
+}
