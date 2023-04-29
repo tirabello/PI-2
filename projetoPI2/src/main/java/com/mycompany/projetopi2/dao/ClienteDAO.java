6adbe7c7e23ce89b849b9a26664317115a6e02b9
Leonardo Silva <ldss1@outlook.com>
2023-04-29 11:27:37 -0300

    SQL QUERY DELETE CLIENTE

--- projetoPI2/src/main/java/com/mycompany/projetopi2/dao/ClienteDAO.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/dao/ClienteDAO.java
@@ -22,125 +22,145 @@ import java.util.logging.Logger;
  */
 public class ClienteDAO {
 
-	private static boolean status = false;
-	private static Connection conexao = null;
-	private static PreparedStatement instrucaoSQL = null;
+    private static boolean status = false;
+    private static Connection conexao = null;
+    private static PreparedStatement instrucaoSQL = null;
+    private static String query;
 
+    // Adicionar Cliente
+    public static boolean adicionarCliente(Cliente objCli) {
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
 
-	// Adicionar Cliente
-	public static boolean adicionarCliente(Cliente objCli) {
-		try {
-			conexao = GerenciadorConexao.abrirConexao();
+            query = "INSERT INTO cliente ( Nome, Sexo, DataNascimento, EstadoCivil, CPF, Telefone, Email, Endereco ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )";
 
-			String query = "INSERT INTO cliente ( Nome, Sexo, DataNascimento, EstadoCivil, CPF, Telefone, Email, Endereco ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? )";
+            instrucaoSQL = conexao.prepareStatement(query);
 
+            instrucaoSQL.setString(1, objCli.getNome());
+            instrucaoSQL.setString(2, objCli.getSexo());
+            instrucaoSQL.setDate(3, new java.sql.Date(objCli.getDataNascimento().getTime()));
+            instrucaoSQL.setString(4, objCli.getEstadoCivil());
+            instrucaoSQL.setString(5, objCli.getCpf());
+            instrucaoSQL.setString(6, objCli.getTelefone());
+            instrucaoSQL.setString(7, objCli.getEmail());
+            instrucaoSQL.setString(8, objCli.getEndereco());
 
-			instrucaoSQL = conexao.prepareStatement(query);
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
 
-			instrucaoSQL.setString(1, objCli.getNome());
-			instrucaoSQL.setString(2, objCli.getSexo());
-			instrucaoSQL.setDate(3, new java.sql.Date(objCli.getDataNascimento().getTime()));
-			instrucaoSQL.setString(4, objCli.getEstadoCivil());
-			instrucaoSQL.setString(5, objCli.getCpf());
-			instrucaoSQL.setString(6, objCli.getTelefone());
-			instrucaoSQL.setString(7, objCli.getEmail());
-			instrucaoSQL.setString(8, objCli.getEndereco());
-
-
-			int linhasAfetadas = instrucaoSQL.executeUpdate();
-
-			if (linhasAfetadas > 0) {
-				status = true;
-			} else {
-				status = false;
-			}
-
-
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return status;
-	}
-	// Editar Cliente
-
-	public static boolean atualizarCliente(Cliente objCli) {
-		try {
-			conexao = GerenciadorConexao.abrirConexao();
-
-			String query = "UPDATE cliente SET Nome = ?, Sexo = ?, DataNascimento = ?, EstadoCivil = ?, CPF = ?, Telefone = ?, Email = ?, Endereco = ? WHERE id_cliente = ?";
-
-			instrucaoSQL = conexao.prepareStatement(query);
-
-			instrucaoSQL.setString(1, objCli.getNome());
-			instrucaoSQL.setString(2, objCli.getSexo());
-			instrucaoSQL.setDate(3, new java.sql.Date(objCli.getDataNascimento().getTime()));
-			instrucaoSQL.setString(4, objCli.getEstadoCivil());
-			instrucaoSQL.setString(5, objCli.getCpf());
-			instrucaoSQL.setString(6, objCli.getTelefone());
-			instrucaoSQL.setString(7, objCli.getEmail());
-			instrucaoSQL.setString(8, objCli.getEndereco());
-			instrucaoSQL.setInt(9, objCli.getId_cliente());
-
-			instrucaoSQL.executeUpdate();
-
-			status = true;
-
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-                return status;
-	}
-
-	// Listar Cliente
-	public static ArrayList<Cliente> listarClientes() {
-                ArrayList<Cliente> listaClientes = new ArrayList<>();
-            try {
-                conexao = GerenciadorConexao.abrirConexao();
-
-				String query = "SELECT * FROM cliente";
-
-				instrucaoSQL = conexao.prepareStatement(query);
-
-				ResultSet rs = instrucaoSQL.executeQuery();
-
-				if (rs != null) {
-					while(rs.next()) {
-						Cliente objCli = new Cliente();
-						objCli.setId_cliente(rs.getInt("IDCliente"));
-						objCli.setNome(rs.getString("Nome"));
-						objCli.setSexo(rs.getString("Sexo"));
-						objCli.setDataNascimento(rs.getDate("DataNascimento"));
-						objCli.setEstadoCivil(rs.getString("EstadoCivil"));
-						objCli.setCpf(rs.getString("CPF"));
-						objCli.setTelefone(rs.getString("Telefone"));
-						objCli.setEmail(rs.getString("Email"));
-						objCli.setEndereco(rs.getString("Endereco"));
-
-						listaClientes.add(objCli);
-					}
-				}
-                
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SQLException ex) {
-                Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            if (linhasAfetadas > 0) {
+                status = true;
+            } else {
+                status = false;
             }
-		return listaClientes;
-	}
 
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return status;
+    }
+    // Editar Cliente
 
-	// Excluir Cliente
-	// Buscar Cliente
+    public static boolean atualizarCliente(Cliente objCli) {
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
 
+            query = "UPDATE cliente SET Nome = ?, Sexo = ?, DataNascimento = ?, EstadoCivil = ?, CPF = ?, Telefone = ?, Email = ?, Endereco = ? WHERE id_cliente = ?";
 
+            instrucaoSQL = conexao.prepareStatement(query);
+
+            instrucaoSQL.setString(1, objCli.getNome());
+            instrucaoSQL.setString(2, objCli.getSexo());
+            instrucaoSQL.setDate(3, new java.sql.Date(objCli.getDataNascimento().getTime()));
+            instrucaoSQL.setString(4, objCli.getEstadoCivil());
+            instrucaoSQL.setString(5, objCli.getCpf());
+            instrucaoSQL.setString(6, objCli.getTelefone());
+            instrucaoSQL.setString(7, objCli.getEmail());
+            instrucaoSQL.setString(8, objCli.getEndereco());
+            instrucaoSQL.setInt(9, objCli.getId_cliente());
+
+            instrucaoSQL.executeUpdate();
+
+            status = true;
+
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    // Listar Cliente
+    public static ArrayList<Cliente> listarClientes() {
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+
+            query = "SELECT * FROM cliente";
+
+            instrucaoSQL = conexao.prepareStatement(query);
+
+            ResultSet rs = instrucaoSQL.executeQuery();
+
+            if (rs != null) {
+                while (rs.next()) {
+                    Cliente objCli = new Cliente();
+                    objCli.setId_cliente(rs.getInt("IDCliente"));
+                    objCli.setNome(rs.getString("Nome"));
+                    objCli.setSexo(rs.getString("Sexo"));
+                    objCli.setDataNascimento(rs.getDate("DataNascimento"));
+                    objCli.setEstadoCivil(rs.getString("EstadoCivil"));
+                    objCli.setCpf(rs.getString("CPF"));
+                    objCli.setTelefone(rs.getString("Telefone"));
+                    objCli.setEmail(rs.getString("Email"));
+                    objCli.setEndereco(rs.getString("Endereco"));
+
+                    listaClientes.add(objCli);
+                }
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaClientes;
+    }
+
+    // Excluir Cliente
+    public static boolean removerCliente(int idCliente) {
+
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+
+            query = "DELETE FROM cliente WHERE IDCliente = ?";
+            instrucaoSQL = conexao.prepareStatement(query);
+
+            instrucaoSQL.setInt(1, idCliente);
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas > 0) {
+                status = true;
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status;
+    }
+
+    // Buscar Cliente por CPF
+    
+    // Buscar Cliente por NOME
     
 }
