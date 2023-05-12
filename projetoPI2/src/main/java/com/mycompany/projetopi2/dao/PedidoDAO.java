108a4c07ec1d76163f56e9474bfaeda372a24438
Leonardo Silva <ldss1@outlook.com>
2023-05-12 00:07:46 -0300

    New Version antes de refatoracao

--- projetoPI2/src/main/java/com/mycompany/projetopi2/dao/PedidoDAO.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/dao/PedidoDAO.java
@@ -8,7 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import com.mycompany.projetopi2.models.Pedido;
+import com.mycompany.projetopi2.models.Venda;
+import com.mycompany.projetopi2.models.Produto;
 import com.mycompany.projetopi2.utils.GerenciadorConexao;
 
 /**
@@ -17,64 +18,71 @@ import com.mycompany.projetopi2.utils.GerenciadorConexao;
  */
 public class PedidoDAO {
 
-	private static boolean status = false;
-	private static Connection conexao = null;
-	private static PreparedStatement instrucaoSQL = null;
-	private static String query;
+    private static boolean status = false;
+    private static Connection conexao = null;
+    private static PreparedStatement instrucaoSQL = null;
+    private static String query;
 
-	public static boolean salvarPedido(Pedido objPed) {
-		
-		try {
-			conexao = GerenciadorConexao.abrirConexao();
-			query = "INSERT INTO Pedido (fk_IDCliente, DataPedido, HoraPedido, Quantidade, ValorTotal) VALUES (?, ?, ?, ?, ?)";
+    public static boolean salvarPedido(Venda objPed) {
 
-			instrucaoSQL = conexao.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-			instrucaoSQL.setInt(1, objPed.getIdCliente());
-			instrucaoSQL.setString(2, new java.sql.Date(objPed.getData().getTime()).toString());
-			instrucaoSQL.setString(3, new java.sql.Time(objPed.getHora().getTime()).toString());
-			instrucaoSQL.setInt(4, objPed.getQuantidade());
-			instrucaoSQL.setDouble(5, objPed.getValorTotal());
+        try {
+            conexao = GerenciadorConexao.abrirConexao();
+            query = "INSERT INTO Pedido (fk_IDCliente, DataPedido, HoraPedido, Quantidade, ValorTotal) VALUES (?, ?, ?, ?, ?)";
 
-			instrucaoSQL.executeUpdate();
+            instrucaoSQL = conexao.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            instrucaoSQL.setInt(1, objPed.getIdCliente());
+            instrucaoSQL.setDate(2, new java.sql.Date(objPed.getData().getTime()));
+            instrucaoSQL.setTime(3, new java.sql.Time(objPed.getHora().getTime()));
+            instrucaoSQL.setInt(4, objPed.getQntItens());
+            instrucaoSQL.setDouble(5, objPed.getValorTotal());
 
-			int linhasAfetadas = instrucaoSQL.executeUpdate();
+            instrucaoSQL.executeUpdate();
 
-			if (linhasAfetadas > 0){
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
 
-				ResultSet rs = instrucaoSQL.getGeneratedKeys();
+            if (linhasAfetadas > 0) {
 
-				int IDVenda = rs.getInt(1);
-				System.out.println("ID Venda: " + IDVenda);
-				
-				for (int i = 0; i < objPed.getListaProdutos().size(); i++) {
-					query = "INSERT INTO ItemPedido (fk_IDPedido, NumItem, fk_IDProduto, Preco, Quantidade) VALUES (?, ?, ?, ?, ?)";
-					instrucaoSQL = conexao.prepareStatement(query);
+                ResultSet rs = instrucaoSQL.getGeneratedKeys();
 
-					instrucaoSQL.setInt(1, IDVenda);
-					instrucaoSQL.setInt(2, i + 1);
-					instrucaoSQL.setInt(3, objPed.getListaProdutos().get(i).getIdProduto());
-					instrucaoSQL.setDouble(4, objPed.getListaProdutos().get(i).getValor());
-					instrucaoSQL.setInt(5, objPed.getListaProdutos().get(i).getQuantidade());
+                if (rs.next()) {
+                    int idPedido = rs.getInt(1);
+                    int IDVenda = rs.getInt(1);
+                    System.out.println("ID Venda: " + IDVenda);
 
-					instrucaoSQL.executeUpdate();
-				}
+                    // fOR EACH
+                    query = "INSERT INTO ItemPedido (fk_IDPedido, NumItem, fk_IDProduto, Preco, Quantidade) VALUES (?, ?, ?, ?, ?)";
+                    int i = 0;
+                    for (Produto item : objPed.getListaProdutos()) {
 
-				status = true;
+                        instrucaoSQL = conexao.prepareStatement(query);
 
-			} else {
-				System.out.println("Erro ao cadastrar venda!");
-			}
+                        instrucaoSQL.setInt(1, IDVenda);
+                        instrucaoSQL.setInt(2, i++);
 
-			status = true;
-		} catch (Exception e) {
-			System.out.println("Erro ao cadastrar venda: " + e.getMessage());
-		}
-		return status;
+                        instrucaoSQL.setInt(3, item.getIdProduto());
+                        instrucaoSQL.setDouble(4, item.getValor());
+                        instrucaoSQL.setInt(5, item.getQuantidade());
 
-	}
+                        instrucaoSQL.executeUpdate();
+                    }
 
-	// Adicionar Pedido
-	// Buscar Pedido
-	// Listar Pedido
-    
+                }
+
+                status = true;
+
+            } else {
+                System.out.println("Erro ao cadastrar venda!");
+            }
+
+            status = true;
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar venda: " + e.getMessage());
+        }
+        return status;
+
+    }
+
+    // Adicionar Venda
+    // Buscar Venda
+    // Listar Venda
 }
