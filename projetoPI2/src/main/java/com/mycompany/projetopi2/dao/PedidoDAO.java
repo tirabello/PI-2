cb33d6da505be8cbff0be889faf14b8eac746d08
Leonardo Silva <ldss1@outlook.com>
2023-05-12 00:31:01 -0300

    New Version mudanca de classes

--- projetoPI2/src/main/java/com/mycompany/projetopi2/dao/PedidoDAO.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/dao/PedidoDAO.java
@@ -5,11 +5,12 @@
 package com.mycompany.projetopi2.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import com.mycompany.projetopi2.models.Venda;
-import com.mycompany.projetopi2.models.Produto;
+import com.mycompany.projetopi2.models.ItemVenda;
 import com.mycompany.projetopi2.utils.GerenciadorConexao;
 
 /**
@@ -23,18 +24,17 @@ public class PedidoDAO {
     private static PreparedStatement instrucaoSQL = null;
     private static String query;
 
-    public static boolean salvarPedido(Venda objPed) {
+    public static boolean salvarPedido(Venda venda) {
 
         try {
             conexao = GerenciadorConexao.abrirConexao();
-            query = "INSERT INTO Pedido (fk_IDCliente, DataPedido, HoraPedido, Quantidade, ValorTotal) VALUES (?, ?, ?, ?, ?)";
+            query = "INSERT INTO Pedido (fk_IDCliente, DataHora, QntItens, ValorTotal) VALUES (?, ?, ?, ?)";
 
             instrucaoSQL = conexao.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-            instrucaoSQL.setInt(1, objPed.getIdCliente());
-            instrucaoSQL.setDate(2, new java.sql.Date(objPed.getData().getTime()));
-            instrucaoSQL.setTime(3, new java.sql.Time(objPed.getHora().getTime()));
-            instrucaoSQL.setInt(4, objPed.getQntItens());
-            instrucaoSQL.setDouble(5, objPed.getValorTotal());
+            instrucaoSQL.setInt(1, venda.getIdCliente());
+            instrucaoSQL.setDate(2, java.sql.Date.valueOf(venda.getDataHora().toLocalDate()));
+            instrucaoSQL.setInt(3, venda.getQntItens());
+            instrucaoSQL.setDouble(4, venda.getValorTotal());
 
             instrucaoSQL.executeUpdate();
 
@@ -45,23 +45,22 @@ public class PedidoDAO {
                 ResultSet rs = instrucaoSQL.getGeneratedKeys();
 
                 if (rs.next()) {
-                    int idPedido = rs.getInt(1);
-                    int IDVenda = rs.getInt(1);
-                    System.out.println("ID Venda: " + IDVenda);
+                    int idVenda = rs.getInt(1);
+                    System.out.println("ID Venda: " + idVenda);
 
                     // fOR EACH
-                    query = "INSERT INTO ItemPedido (fk_IDPedido, NumItem, fk_IDProduto, Preco, Quantidade) VALUES (?, ?, ?, ?, ?)";
-                    int i = 0;
-                    for (Produto item : objPed.getListaProdutos()) {
+                    query = "INSERT INTO ItemPedido (fk_IDPedido, NumItem, fk_IDProduto, ValorUnitario, Quantidade) VALUES (?, ?, ?, ?, ?)";
+
+                    for (ItemVenda item : venda.getListaProdutos()) {
 
                         instrucaoSQL = conexao.prepareStatement(query);
 
-                        instrucaoSQL.setInt(1, IDVenda);
-                        instrucaoSQL.setInt(2, i++);
+                        instrucaoSQL.setInt(1, idVenda);
+                        instrucaoSQL.setInt(2, item.getNumItem());
 
                         instrucaoSQL.setInt(3, item.getIdProduto());
-                        instrucaoSQL.setDouble(4, item.getValor());
-                        instrucaoSQL.setInt(5, item.getQuantidade());
+                        instrucaoSQL.setDouble(4, item.getVlrUnitario());
+                        instrucaoSQL.setInt(5, item.getQntProduto());
 
                         instrucaoSQL.executeUpdate();
                     }
