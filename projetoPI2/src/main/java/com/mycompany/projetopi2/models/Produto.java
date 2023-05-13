e57f07e02d7be91186a037663b053f2047cef0fa
Leonardo Silva <ldss1@outlook.com>
2023-05-13 04:11:41 -0300

    New Update

--- projetoPI2/src/main/java/com/mycompany/projetopi2/models/Produto.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/Produto.java
@@ -4,116 +4,97 @@ package com.mycompany.projetopi2.models;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-
 /**
  *
  * @author ldss1
  */
 public class Produto {
 
-	private int idProduto;
+    private int idProduto;
     private String nome;
-	private String codProduto;
-	private double valor;
+    private String codProduto;
+    private double valor;
     private String categoria;
-	private String unidadeVenda;
-	private int quantidade;
-	private String descricao;
+    private String unidadeVenda;
+    private int quantidade;
+    private String descricao;
 
+    public Produto() {
+    }
 
-	public Produto() {
-	}
+    public Produto(String nome, String codProduto, double valor, String categoria,
+            String unidadeVenda, int quantidade, String descricao) {
+        this.nome = nome;
+        this.codProduto = codProduto;
+        this.valor = valor;
+        this.categoria = categoria;
+        this.unidadeVenda = unidadeVenda;
+        this.quantidade = quantidade;
+        this.descricao = descricao;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
-	public Produto(String nome, String codProduto, double valor, String categoria,
-			String unidadeVenda, int quantidade, String descricao) {
-		this.nome = nome;
-		this.codProduto = codProduto;
-		this.valor = valor;
-		this.categoria = categoria;
-		this.unidadeVenda = unidadeVenda;
-		this.quantidade = quantidade;
-		this.descricao = descricao;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	
-	public String getNome() {
-		return nome;
-	}
+    public String getCodProduto() {
+        return codProduto;
+    }
 
+    public void setCodProduto(String codProduto) {
+        this.codProduto = codProduto;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public double getValor() {
+        return valor;
+    }
 
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 
-	public String getCodProduto() {
-		return codProduto;
-	}
+    public String getCategoria() {
+        return categoria;
+    }
 
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 
-	public void setCodProduto(String codProduto) {
-		this.codProduto = codProduto;
-	}
+    public String getUnidadeVenda() {
+        return unidadeVenda;
+    }
 
+    public void setUnidadeVenda(String unidadeVenda) {
+        this.unidadeVenda = unidadeVenda;
+    }
 
-	public double getValor() {
-		return valor;
-	}
+    public int getQuantidade() {
+        return quantidade;
+    }
 
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	public String getCategoria() {
-		return categoria;
-	}
+    public int getIdProduto() {
+        return idProduto;
+    }
 
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-
-
-	public String getUnidadeVenda() {
-		return unidadeVenda;
-	}
-
-
-	public void setUnidadeVenda(String unidadeVenda) {
-		this.unidadeVenda = unidadeVenda;
-	}
-
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-
-	public int getIdProduto() {
-		return idProduto;
-	}
-
-
-	public void setIdProduto(int idProduto) {
-		this.idProduto = idProduto;
-	}
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
 
 }
