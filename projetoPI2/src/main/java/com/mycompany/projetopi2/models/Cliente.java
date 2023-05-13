0cbd8e4a6ddd7db431cd0f34365ad3a9dfc20923
Leonardo Silva <ldss1@outlook.com>
2023-05-13 15:09:49 -0300

    Cliente - Model

--- projetoPI2/src/main/java/com/mycompany/projetopi2/models/Cliente.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/Cliente.java
@@ -11,136 +11,119 @@ import java.util.Date;
  * @author ldss1
  */
 public class Cliente {
-	private int id_cliente;
+
+    private int id_cliente;
     private String nome;
-	private String sexo;
-	private Date dataNascimento;
-	private String estadoCivil;
-	private String cpf;
-	private String telefone;
-	private String email;
-	private String endereco;
+    private String sexo;
+    private Date dataNascimento;
+    private String estadoCivil;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String endereco;
 
-	
-	public Cliente() {
-	}
+    public Cliente() {
+    }
 
+    public Cliente(String nome, String sexo, Date dataNascimento, String estadoCivil, String cpf, String telefone,
+            String email, String endereco) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.estadoCivil = estadoCivil;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
 
-	public Cliente(String nome, String sexo, Date dataNascimento, String estadoCivil, String cpf, String telefone,
-			String email, String endereco) {
-		this.nome = nome;
-		this.sexo = sexo;
-		this.dataNascimento = dataNascimento;
-		this.estadoCivil = estadoCivil;
-		this.cpf = cpf;
-		this.telefone = telefone;
-		this.email = email;
-		this.endereco = endereco;
-	}
+    public Cliente(int id_cliente, String nome, String sexo, Date dataNascimento, String estadoCivil, String cpf,
+            String telefone, String email, String endereco) {
+        this.id_cliente = id_cliente;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.estadoCivil = estadoCivil;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
 
+    public int getId_cliente() {
+        return id_cliente;
+    }
 
-	public Cliente(int id_cliente, String nome, String sexo, Date dataNascimento, String estadoCivil, String cpf,
-			String telefone, String email, String endereco) {
-		this.id_cliente = id_cliente;
-		this.nome = nome;
-		this.sexo = sexo;
-		this.dataNascimento = dataNascimento;
-		this.estadoCivil = estadoCivil;
-		this.cpf = cpf;
-		this.telefone = telefone;
-		this.email = email;
-		this.endereco = endereco;
-	}
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
-	public int getId_cliente() {
-		return id_cliente;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getSexo() {
+        return sexo;
+    }
 
-	public void setId_cliente(int id_cliente) {
-		this.id_cliente = id_cliente;
-	}
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
 
-	public String getSexo() {
-		return sexo;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
+    public String getTelefone() {
+        return telefone;
+    }
 
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public String getEndereco() {
+        return endereco;
+    }
 
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
+    public int getIdCliente() {
+        return id_cliente;
+    }
 
-	public String getEstadoCivil() {
-		return estadoCivil;
-	}
-
-
-	public void setEstadoCivil(String estadoCivil) {
-		this.estadoCivil = estadoCivil;
-	}
-
-
-	public String getCpf() {
-		return cpf;
-	}
-
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	
 }
