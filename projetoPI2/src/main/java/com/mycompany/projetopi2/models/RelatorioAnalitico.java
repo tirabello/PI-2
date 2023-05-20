556003dfb19317f005e2812f9a9d734b637a95dd
Leonardo Silva <ldss1@outlook.com>
2023-05-20 02:23:50 -0300

    Create Class Model - Relatorio Analitico

---
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/RelatorioAnalitico.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetopi2.models;
+
+/**
+ *
+ * @author ldss1
+ */
+public class RelatorioAnalitico {
+    private int numVenda;
+	private int numItem;
+	private String Produto;
+	private double vlrUnitario;
+	private int qtdVendida;
+	private double vlrTotal;
+
+	public RelatorioAnalitico() {
+	}
+
+	public RelatorioAnalitico(int numVenda, int numItem, String produto, double vlrUnitario, int qtdVendida, double vlrTotal) {
+		this.numVenda = numVenda;
+		this.numItem = numItem;
+		this.Produto = produto;
+		this.vlrUnitario = vlrUnitario;
+		this.qtdVendida = qtdVendida;
+		this.vlrTotal = vlrTotal;
+	}
+
+	public int getNumVenda() {
+		return numVenda;
+	}
+
+	public void setNumVenda(int numVenda) {
+		this.numVenda = numVenda;
+	}
+
+	public int getNumItem() {
+		return numItem;
+	}
+
+	public void setNumItem(int numItem) {
+		this.numItem = numItem;
+	}
+
+	public String getProduto() {
+		return Produto;
+	}
+
+	public void setProduto(String produto) {
+		Produto = produto;
+	}
+
+	public double getVlrUnitario() {
+		return vlrUnitario;
+	}
+
+	public void setVlrUnitario(double vlrUnitario) {
+		this.vlrUnitario = vlrUnitario;
+	}
+
+	public int getQtdVendida() {
+		return qtdVendida;
+	}
+
+	public void setQtdVendida(int qtdVendida) {
+		this.qtdVendida = qtdVendida;
+	}
+
+	public double getVlrTotal() {
+		return vlrTotal;
+	}
+
+	public void setVlrTotal(double vlrTotal) {
+		this.vlrTotal = vlrTotal;
+	}
+	
+
+}
