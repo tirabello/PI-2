10f72b56c39929ed1d31158a370fcea53d489e58
Leonardo Silva <ldss1@outlook.com>
2023-05-21 02:04:11 -0300

    Final Version

--- projetoPI2/src/main/java/com/mycompany/projetopi2/models/Produto.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/Produto.java
@@ -1,13 +1,5 @@
 package com.mycompany.projetopi2.models;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author ldss1
- */
 public class Produto {
 
     private int idProduto;
