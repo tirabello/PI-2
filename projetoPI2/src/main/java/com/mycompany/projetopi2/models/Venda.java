46d1d0030fc8878777dd3b582314b1996dc7ca78
Leonardo Silva <ldss1@outlook.com>
2023-05-21 02:18:55 -0300

    Final Version

--- projetoPI2/src/main/java/com/mycompany/projetopi2/models/Venda.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/Venda.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projetopi2.models;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- *
- * @author ldss1
- */
 public class Venda {
 
     private int idVenda;
