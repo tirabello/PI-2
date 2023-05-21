598fac5fa3a097ec1ddcaaeadc7dee06f92d7096
Leonardo Silva <ldss1@outlook.com>
2023-05-21 01:56:21 -0300

    Final Version

--- projetoPI2/src/main/java/com/mycompany/projetopi2/models/Cliente.java
+++ projetoPI2/src/main/java/com/mycompany/projetopi2/models/Cliente.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projetopi2.models;
 
 import java.util.Date;
 
-/**
- *
- * @author ldss1
- */
 public class Cliente {
 
     private int id_cliente;
